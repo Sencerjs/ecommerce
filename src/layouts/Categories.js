14488ec018b938a8d164fa09dfb2388d5821878a
Sencerjs <84234970+Sencerjs@users.noreply.github.com>
2021-06-06 00:28:13 +0300

    06jul0001

---
+++ src/layouts/Categories.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Input, Label, Menu } from 'semantic-ui-react'
+
+export default function Categories() {
+    return (
+        <div>
+              <Menu vertical>
+        <Menu.Item
+          name='inbox'
+         
+        >
+          <Label color='teal'>1</Label>
+          Inbox
+        </Menu.Item>
+
+        <Menu.Item
+          name='spam'
+        
+        >
+          <Label>51</Label>
+          Spam
+        </Menu.Item>
+
+        <Menu.Item
+          name='updates'
+      
+        >
+          <Label>1</Label>
+          Updates
+        </Menu.Item>
+        <Menu.Item>
+          <Input icon='search' placeholder='Search mail...' />
+        </Menu.Item>
+      </Menu>
+        </div>
+    )
+}
